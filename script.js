4b2477edb54194feaf8e7953398b8a724a70a530
Michelle L. Wu <micleewu@gmail.com>
2024-07-10 14:27:10 -0700

    Made sidebar transparent and auto-close on mouse leave

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.getElementById("sidebar").classList.toggle("active");
 });
+
+document.getElementById("sidebar").addEventListener("mouseleave", function() {
+    document.getElementById("sidebar").classList.remove("active");
+});
